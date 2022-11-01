fb9b6c8c7758f63087515b4d5a2589bb0d86ca8c
Ashish sharma <108014986+Ashish0955@users.noreply.github.com>
2022-11-01 09:21:30 +0530

    1 nov

---
+++ script.js
@@ -0,0 +1,18 @@
+var index = 0;
+
+function changeColors() {
+    var colors = ["red", "blue", "orange", "yellow", "green", "purple"];
+    
+    
+    document.getElementsByTagName("body")[0].
+    
+        style.background = colors[index++];
+    
+    // console.log(index);
+    if (index > colors.length - 1)
+        index = 0;
+}
+
+
+a = document.getElementsByTagName("body")[0];
+console.log(a);
